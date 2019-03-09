a1cce32c52339b4a1067fad8c436da14483dafdb
Ubuntu <ubuntu@ip-172-31-25-33.us-east-2.compute.internal>
2019-03-09 15:50:52 +0000

    Bugfix

--- Api/api_fetchers.js
+++ Api/api_fetchers.js
@@ -53,7 +53,7 @@ module.exports = {
               axios.get(URL.basePath+URL.leagueBySummonerId+summonerId+"?api_key="+riotApiToken).then(function(response) {
                 callback(response); 
               }).catch(error => {
-                     // console.error("Fetch league error:", error.response)
+                      console.error("Fetch league error:", error.response)
                       callback(error.response)
               })
       },
@@ -62,8 +62,7 @@ module.exports = {
         axios.get(URL.basePath+URL.activeGameSpectator+summonerId+"?api_key="+riotApiToken).then(function(response) { 
                 callback(response); 
         }).catch(error => {
-                console.error("Fetch active match error: ", error.response); 
-                callback(error.response); 
+                console.error("Fetch active match error: ", error.response.status); 
         })
       },
 
